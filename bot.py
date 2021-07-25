57ea4c304f5b0fe7196cf1251c9987bdfd315815
myejin <rlagpwlsq78@gmail.com>
2021-07-26 00:37:07 +0900

    AWS Lambda함수로 변경
    테스트 완료, 유투브 쿼리문에 특정키워드 제외하는 건 없는지 확인해보기
    API Quata 초과 에러클래스 있는지 확인해보기
    람다함수 트리거 더 공부하기, python anywhere 사용이 불가피한지 알아보기

--- bot.py
+++ bot.py
@@ -1,51 +1,45 @@
 import requests
-from pprint import pprint
-import config
 import random
-from telegram.ext import Updater, MessageHandler, Filters
-
-video_list = []
+import os
 
 
-def handler(update, context):
-    user_text = update.message.text
-    if user_text == "네" or user_text == "다시":
+def lambda_handler(event, context):
+    try:
+        user_text = event["result"]["text"]
+        if user_text == "네" or user_text == "다시":
+            video_list = crawl_url()
+            send_message(video_list)
+
+    except Exception:
         send_message()
 
 
-def bot_updater():
-    token = config.CONFIG["BOT_TOKEN"]
-    updater = Updater(token=token, use_context=True)
-    updater.start_polling()
-    echo_handler = MessageHandler(Filters.text, handler)
-    updater.dispatcher.add_handler(echo_handler)
+def send_message(video_list=None):
+    token = os.environ["BOT_TOKEN"]
+    chat_id = os.environ["ChatId"]
 
-
-def send_message():
-    global video_list
-    idx = random.choice(range(len(video_list)))
-
-    token = config.CONFIG["BOT_TOKEN"]
-    chat_id = config.CONFIG["ChatId"]
-
-    msg = "✨오늘 저녁메뉴 추천✨\n\n🍳" + video_list[idx] + "\n\n메뉴를 다시 찾아볼까요?🥺\n('네' 또는 '다시' 입력)"
+    msg = ""
+    if video_list is None:
+        msg = f"오늘 조회가능한 횟수를 초과했어요!!"
+    else:
+        idx = random.choice(range(len(video_list)))
+        msg = "✨오늘 저녁메뉴 추천✨\n\n🍳" + video_list[idx] + "\n\n메뉴를 다시 찾아볼까요?🥺\n('네' 또는 '다시' 입력)\n"
     url = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={msg}"
     resp = requests.get(url)
 
 
 def crawl_url():
-    channel_id = "UCyn-K7rZLXjGl7VXGweIlcA"
-    api_key = config.CONFIG["KEY"]
-    nextPageToken = ""
     video_list = []
+    api_key = os.environ["KEY"]
+    nextPageToken = ""
     finished = False
+
     while not finished:
-        url = f"https://www.googleapis.com/youtube/v3/search?key={api_key}&part=id&channelId={channel_id}&maxResults=100&q=간단+재료&type=video"
+        url = f"https://www.googleapis.com/youtube/v3/search?key={api_key}&part=id&channelId=UCyn-K7rZLXjGl7VXGweIlcA&maxResults=100&q=간단+재료&type=video"
         if nextPageToken:
             url += f"&pageToken={nextPageToken}"
 
         resp = requests.get(url).json()
-        pprint(resp)
         items = resp["items"]
         if "nextPageToken" in resp:
             nextPageToken = resp["nextPageToken"]
@@ -56,10 +50,4 @@ def crawl_url():
             videoId = item["id"]["videoId"]
             url = f"https://www.youtube.com/watch?v={videoId}"
             video_list.append(url)
-
     return video_list
-
-
-if __name__ == "__main__":
-    video_list = crawl_url()
-    bot_updater()
